4a4fd484b5e7e7c94ea3c8bea2be9eaf0bc4d5ad
BivekJoshi01 <Bivek.joshi@legalremit.com>
2025-03-23 11:56:08 +0545

    landing Implimentation

--- src/pages/Auth/Login/LoginPage.tsx
+++ src/pages/Auth/Login/LoginPage.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  
   return (
     <div className="">
-      <div className=" p-8 rounded-lg shadow-lg w-96">
+      <div className="p-8 rounded-lg shadow-lg w-96">
         <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
-        <form>
+        <form autoComplete="off">
           <div className="mb-4">
             <label htmlFor="username" className="block text-sm font-semibold text-gray-700">Username</label>
             <input
               type="text"
-              id="username"
               name="username"
+              autoComplete="off"
               className="w-full mt-2 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
               placeholder="Enter your username"
             />
@@ -20,14 +23,14 @@ const LoginPage = () => {
             <label htmlFor="password" className="block text-sm font-semibold text-gray-700">Password</label>
             <input
               type="password"
-              id="password"
               name="password"
+              autoComplete="off"
               className="w-full mt-2 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
               placeholder="Enter your password"
             />
           </div>
           <button
-            type="submit"
+            onClick={() => navigate("Menu/Home")}
             className="w-full bg-blue-500 text-white py-2 rounded-md mt-4 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
           >
             Login
